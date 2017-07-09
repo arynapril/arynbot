83680ba22e45ae7b00dd185aeb4518ca45102c8a
Aryn Thomas <arynapril@gmail.com>
2017-07-09 01:39:05 -0400

    added love command

---
+++ commands/love.js
@@ -0,0 +1,43 @@
+exports.run = (bot, message, args, level) => {
+    var list = Array(
+        "I appreciate you",
+        "You are the most perfect you there is.",
+        "You are enough",
+        "You're strong",
+        "You deserve good things.",
+        "You should be proud of yourself.",
+        "You are good enough.",
+        "You're making a difference.",
+        "Colors seem brighter when you're around",
+        "You're one of a kind",
+        "You're inspiring.",
+        "The world is better because you're in it.",
+        "You're awesome.",
+        "You're totally rad.",
+        "It's going to be alright.",
+        "You're doing great.",
+        "You are loved.",
+        "It's going to be okay.",
+        "No matter how far down you are, it's going to be just fine.",
+        "I know life can be hard, but you can get through it.",
+        "I have total faith in you.",
+        "There are people who care about you.",
+        "You're not alone.",
+        "You deserve the world.",
+        "You deserve love"
+    );
+    message.reply(`${list[Math.floor(Math.random()*list.length)]}`);
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'love',
+    description:  'Spread the love!',
+    usage: 'love'
+};
