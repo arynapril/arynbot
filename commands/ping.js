42423535a66e8206b70e32f615b275221b58dd98
Aryn Thomas <arynapril@gmail.com>
2017-07-10 19:09:43 -0400

    got the two parts mixed up WHOOPS

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = async(bot, message, args, level) => {
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! I took ${m.createdTimestamp - message.createdTimestamp} milleseconds to respond! The whole trip took ${Math.round(bot.ping)} milliseconds!`);
+    m.edit(`Pong! I took ${Math.round(bot.ping)} milliseconds to respond! The whole trip took ${m.createdTimestamp - message.createdTimestamp} milliseconds!`);
 };
 
 exports.conf = {
