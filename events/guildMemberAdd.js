75c97331db1237ed18e127221ae0da68d508b58a
Aryn Thomas <arynapril@gmail.com>
2017-07-10 22:11:51 -0400

    event to play on new guild member

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,4 @@
+module.exports = (bot, member) => {
+    member.guild.defaultChannel.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
+    bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`);
+};
