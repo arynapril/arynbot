8ca95bd93170c6da33579d49beab9117a200cde8
arynapril <arynapril@gmail.com>
2017-07-12 01:27:35 -0400

    updated command

--- commands/nicknames.js
+++ commands/nicknames.js
@@ -3,18 +3,19 @@ exports.run = (bot, message, args, level) => {
     message.guild.members.get("226999841358610432").setNickname("Jax/Jayce");
     message.guild.members.get("172551178087956480").setNickname("Abby");
     message.guild.members.get("302309507164405760").setNickname("Set");
+    message.guild.members.get("163136022106865664").setNickname("Moe");
     message.delete();
 }
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['michael', 'jayce', 'jax', 'set', 'abby', 'nick'],
+    aliases: ['michael', 'jayce', 'jax', 'set', 'abby', 'nick', 'moe', 'micheal'],
     permLevel: 0
 };
 
 exports.help = {
     name: 'nickname',
-    description: 'Changes Michael\'s, Abby\'s, Set\'s, and Jax/Jayce\'s nicknames back to normal',
+    description: 'Changes Michael\'s, Abby\'s, Set\'s, Moe\'s, and Jax/Jayce\'s nicknames back to normal',
     usage: 'nickname'
 };
