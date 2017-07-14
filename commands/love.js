5e67f9bbd70a9af1b600ac538ce2c19dd6c55f0f
arynapril <arynapril@gmail.com>
2017-07-13 22:22:18 -0400

    blacklisted me because i dont deserve love lol

--- commands/love.js
+++ commands/love.js
@@ -30,7 +30,11 @@ exports.run = (bot, message, args, level) => {
         message.reply(`${list[Math.floor(Math.random()*list.length)]}`);
     } else {
         let loved = message.mentions.users.array()[0];
-        message.channel.send(`**${loved.username}**, ${list[Math.floor(Math.random()*list.length)]}`)
+        if (loved.username=="arynapril") {
+            message.channel.send("Haha nope!");
+        } else {
+        message.channel.send(`**${loved.username}**, ${list[Math.floor(Math.random()*list.length)]}`);
+        };
     };
 };
 
