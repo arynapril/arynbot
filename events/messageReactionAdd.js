ddcf0699405803591781c709c76526257a4e8a87
arynapril <arynapril@gmail.com>
2017-07-17 22:27:24 -0400

    updates to hall of fame system

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,14 +1,16 @@
 module.exports = (bot, messageReaction, user) => {
     const Discord = require('discord.js');
-    if (messageReaction.emoji.name == "gayalliance" && messageReaction.count == 3) {
+    if (messageReaction.me) return;
+    if (messageReaction.emoji.name == "gayalliance" && messageReaction.count >= 2) {
         let msg = messageReaction.message;
+        msg.react("332135150328479744");
         const embed = new Discord.RichEmbed()
             .setColor(`${msg.member.displayHexColor}`)
+            .setFooter('Hall of Fame 🏆')
             .setTimestamp()
-            .addField('User', `${msg.author.username}`, true)
+            .addField('User', `${msg.member.nickname} (${msg.author.username})`, true)
             .addField('Channel', `${msg.channel.name}`, true)
             .addField('Message', `${msg}`)
-            .setFooter('');
         bot.channels.get('336108682842603520').send('', {embed});
     };
 };
