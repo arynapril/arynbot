3036795015583e82e1fe24f14123c9754d8a916f
arynapril <arynapril@gmail.com>
2017-07-19 17:17:07 -0400

    significant updates to help command

--- commands/help.js
+++ commands/help.js
@@ -1,30 +1,32 @@
 exports.run = (bot, message, params, level) => {
+    const Discord = require('discord.js');
     if (!params[0]) {
         const myCommands = bot.commands.filter(c => c.conf.permLevel <= level);
-        const commandNames = myCommands.keyArray();
-        const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-        let currentCategory = "";
-        let output = `= Command List =\n\n[Use ${bot.config.prefix}help <commandname> for details]\n`;
-        const sorted = myCommands.sort((p, c) => p.help.category > c.help.category);
-        sorted.forEach(c => {
-            if (currentCategory !== c.help.category) {
-                output += `\n== ${c.help.category}\n`;
-                currentCategory = c.help.category;
-            }
-            output += `${bot.config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
-        });
-        message.channel.send(output, {
-            code: 'asciidoc'
-        });
+        var helpbox = new Discord.RichEmbed();
+        helpbox.setTitle("Command List")
+               .setDescription(`Use ${bot.config.prefix}help <commandname> for details`)
+               .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+        myCommands.forEach(c => {
+            helpbox.addField(c.help.name, c.help.description)
+        }
+    );
+        message.channel.send({embed: helpbox});
     } else {
         let command = params[0];
         if (bot.commands.has(command)) {
             command = bot.commands.get(command);
-            message.channel.send(`${command.help.name}\n${command.help.description}\nusage   :: ${bot.config.prefix}${command.help.usage}\naliases :: ${command.conf.aliases.join(', ')}`, {
-                code: 'asciidoc'
-            });
-        }
-    }
+            var helpCommand = new Discord.RichEmbed();
+            helpCommand.setTitle(command.help.name)
+            .addField('Description', `${command.help.description}`)
+            .addField('Usage', `${bot.config.prefix}${command.help.usage}`)
+            .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+            if (command.conf.aliases == "") {
+            } else {
+                helpCommand.addField('Aliases', `${command.conf.aliases.join(', ')}`)
+            }
+            message.channel.send({embed: helpCommand});
+        };
+    };
 };
 exports.conf = {
     enabled: true, 
