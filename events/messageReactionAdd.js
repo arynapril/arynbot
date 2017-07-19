df366925c897e1c6b837de80b5e8a903dc9637d7
arynapril <arynapril@gmail.com>
2017-07-19 14:54:53 -0400

    uped reaction requirement to be hall of famed ;p

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,14 +1,19 @@
 module.exports = (bot, messageReaction, user) => {
     const Discord = require('discord.js');
     if (messageReaction.me) return;
-    if (messageReaction.emoji.name == "gayalliance" && messageReaction.count >= 2) {
+    if (messageReaction.emoji.name == "gayalliance" && messageReaction.count >= 3) {
         let msg = messageReaction.message;
         msg.react("332135150328479744");
+        if (message.member.nickname == null) {
+            let userbox = `${msg.author.username}`;
+        } else {
+            let userbox = `${msg.member.nickname} (${msg.author.username})`;
+        };
         const embed = new Discord.RichEmbed()
             .setColor(`${msg.member.displayHexColor}`)
             .setFooter('Hall of Fame 🏆')
             .setTimestamp()
-            .addField('User', `${msg.member.nickname} (${msg.author.username})`, true)
+            .addField('User', `${userbox}`, true)
             .addField('Channel', `${msg.channel.name}`, true)
             .addField('Message', `${msg}`)
         bot.channels.get('336108682842603520').send('', {embed});
