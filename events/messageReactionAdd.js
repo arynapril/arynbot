dbd1eb967fe4a7c4918957683315d085bcb97741
arynapril <arynapril@gmail.com>
2017-07-20 11:27:06 -0400

    cleaned up HoF message

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,21 +1,21 @@
 module.exports = (bot, messageReaction, user) => {
     const Discord = require('discord.js');
+    const userbox = '';
     if (messageReaction.me) return;
-    if (messageReaction.emoji.name == "gayalliance" && messageReaction.count >= 3) {
+    if (messageReaction.emoji.name == "gayalliance" && messageReaction.count >= 2) {
         let msg = messageReaction.message;
         msg.react("332135150328479744");
-        if (message.member.nickname == null) {
-            let userbox = `${msg.author.username}`;
-        } else {
-            let userbox = `${msg.member.nickname} (${msg.author.username})`;
-        };
-        const embed = new Discord.RichEmbed()
-            .setColor(`${msg.member.displayHexColor}`)
+        const HoF = new Discord.RichEmbed();
+            HoF.setColor(`${msg.member.displayHexColor}`)
             .setFooter('Hall of Fame 🏆')
             .setTimestamp()
-            .addField('User', `${userbox}`, true)
-            .addField('Channel', `${msg.channel.name}`, true)
+        if (msg.member.nickname == null) {
+            HoF.addField('User',`${msg.author.username}`, true)
+        } else {
+            HoF.addField('User',`${msg.member.nickname} (${msg.author.username})`, true);
+        };
+            HoF.addField('Channel', `${msg.channel.name}`, true)
             .addField('Message', `${msg}`)
-        bot.channels.get('336108682842603520').send('', {embed});
+        bot.channels.get('336108682842603520').send({embed: HoF});
     };
 };
