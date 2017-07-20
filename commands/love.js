db0080d288777632182efc923ca8717c0edc4342
arynapril <arynapril@gmail.com>
2017-07-20 00:43:10 -0400

    <3 jax

--- commands/love.js
+++ commands/love.js
@@ -24,7 +24,10 @@ exports.run = (bot, message, args, level) => {
         "there are people who care about you.",
         "you're not alone.",
         "you deserve the world.",
-        "you deserve love."
+        "you deserve love.",
+        "you matter even if you are at your lowest",
+        "you are irreplacable",
+        "you mean the world to someone. Don't take that away from them"
     );
     if (!message.mentions.users.array()[0]) {
         message.reply(`${list[Math.floor(Math.random()*list.length)]}`);
