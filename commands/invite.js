b79fedfb9601a3d8fc447eafb8d1c1195c07fce6
arynapril <arynapril@gmail.com>
2017-07-23 14:39:20 -0400

    added invite command

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+exports.run = (bot, message, args, level) => {
+    bot.generateInvite()
+    .then(link => {
+    message.channel.send(`Invite me to your server using the following link! :heart: ${link}`);
+  });
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'invite',
+    description: 'Provides a link to invite arynbot to your server!',
+    usage: 'invite'
+};
