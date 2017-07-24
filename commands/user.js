22cbf8807082a1d05ef8048c0857091d8c0080a8
arynapril <arynapril@gmail.com>
2017-07-24 10:27:38 -0400

    added userinfo command

---
+++ commands/user.js
@@ -0,0 +1,42 @@
+exports.run = (bot, message, args) => {
+    const Discord = require('discord.js');
+    if (!message.mentions.users.array()[0]) {
+        user = message.author;
+        member = message.member;
+    } else {
+        user = message.mentions.users.array()[0];
+        member = message.mentions.members.array()[0];
+    }
+    var info = new Discord.RichEmbed();
+    if (!user.presence.game) {
+        var game = "None"
+    } else {
+        var game = user.presence.game.name;
+    };
+    info.setTitle(`Info on ${user.username}:`)
+    .setThumbnail(user.avatarURL)
+    .setColor(member.displayHexColor)
+    .addField('Username', user.username, true)
+    .addField('Display Name', member.displayName, true)  
+    .addField('Account Created', user.createdAt, true)
+    .addField('Joined Server', member.joinedAt, true)
+    .addField('Status', user.presence.status, true)
+    .addField('Game', game, true)
+    .addField('Bot', user.bot, true)
+    .addField('Roles', member.roles.array().length-1, true)
+    .addField('Highest Role', member.highestRole.name, true)
+    .addField('Hoist Role', member.hoistRole.name, true)
+    .addField('Color Role', member.colorRole.name, true)
+    message.channel.send({embed: info})
+};
+exports.conf = {
+    enabled: false,
+    guildOnly: false,
+    aliases: ['info'],
+    permLevel: 0
+};
+exports.help = {
+    name: 'user',
+    description: 'Displays assorted user information.',
+    usage: 'user @<users whos info you want to see (optional)>'
+};
