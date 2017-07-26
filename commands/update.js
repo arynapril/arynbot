96e8279669adfd7ffbc69fc5dc8768d3bf4bcdd8
arynapril <arynapril@gmail.com>
2017-07-26 00:19:40 -0400

    edited to work

--- commands/update.js
+++ commands/update.js
@@ -1,23 +1,22 @@
 const child_process = require('child_process');
-exports.run = (bot, msg, args) => {
-    if(msg.author.id != require('../config.json').ownerID) return msg.reply("you are not allowed to do this!") 
-        msg.channel.send("Updating...").then(e => {
-            var update = child_process.execSync('git pull origin').toString()
-            e.channel.send("```" + update + "```")
-            if (update.indexOf("Already up-to-date.") > -1) {
-                e.channel.send("There was nothing to update!");
-            } else {
-                e.channel.send("New code successfully pulled!\nRestarting...")
-                process.exit();
-            }
-        })
+exports.run = (bot, message, args) => {
+    message.channel.send("Updating...").then(e => {
+        var update = child_process.execSync('git pull origin').toString()
+        e.channel.send("```" + update + "```")
+        if (update.indexOf("Already up-to-date.") > -1) {
+            e.channel.send("There was nothing to update!");
+        } else {
+            e.channel.send("New code successfully pulled!\nRestarting...")
+            process.exit();
+        }
+    })
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['upd', 'Update'],
-    permLevel: 0
+    aliases: ['upd', 'Update', 'upd8'],
+    permLevel: 10
 };
 
 exports.help = {
