9f13b2836123c7c13f3f5346c187dfdd6f391080
arynapril <arynapril@gmail.com>
2017-07-28 01:29:38 -0400

    shows some stats about the bot

---
+++ commands/about.js
@@ -0,0 +1,41 @@
+exports.run = (bot, message, args) => {
+    const Discord = require('discord.js');
+    uptime = bot.uptime;
+    time = Math.floor(uptime/1000);
+    if (time >= 3600) { 
+        var hours = Math.floor(x/3600); 
+        var minutes = Math.floor((x%3600)/60); 
+        var seconds = x%60%60;
+        var timecode = `${hours} hours, ${minutes} minutes, and ${seconds} seconds.`
+    } else if (time >= 60) {
+        var minutes = Math.floor(time/60);
+        var seconds = time%60;
+        var timecode = `${minutes} minutes and ${seconds} seconds`;
+    } else {
+        var timecode = `${time} seconds`;
+    }
+    about = new Discord.RichEmbed()
+    .setTitle("About Arynbot")
+    .setDescription("Just a simple bot who is full of love!")
+    .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+    .addField('Botmom', 'arynapril#8549')
+    .addField('Users', bot.users.size, true)
+    .addField('Servers', bot.guilds.size, true)
+    .addField('Channels', bot.channels.size, true)
+    .addField('Uptime', timecode, true)
+    .setThumbnail(bot.user.avatarURL)
+    message.channel.send({embed: about});
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['stats'],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'about',
+    description: 'Rolls a die of a side number up to 9999',
+    usage: 'roll <number of sides, if not included, rolls for a six sided die>'
+};
