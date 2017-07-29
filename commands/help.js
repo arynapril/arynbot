d645902481f2b3ef4c1fe3ffd3939df5b67bb67a
arynapril <arynapril@gmail.com>
2017-07-28 23:58:57 -0400

    fixed help which was apparently broken?

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,14 @@ exports.run = (bot, message, args, level) => {
         const myCommands = bot.commands.filter(c => c.conf.permLevel <= level);
         var helpbox = new Discord.RichEmbed();
         helpbox.setTitle("Command List")
-               .setDescription(`Use ${bot.config.prefix}help <commandname> for details`)
-               .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+            .setDescription(`Use ${bot.config.prefix}help <commandname> for details`)
+            .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
         myCommands.forEach(c => {
             helpbox.addField(c.help.name, c.help.description)
-        }
-    );
-        message.channel.send({embed: helpbox});
+        });
+        message.channel.send({
+            embed: helpbox
+        });
     } else {
         let command = '';
         if (bot.commands.has(args[0])) {
@@ -19,28 +20,27 @@ exports.run = (bot, message, args, level) => {
             command = bot.commands.get(bot.aliases.get(args[0]));
         };
         if (!command) return message.reply(`That command \`${args[0]}\` doesn't seem to exist, nor is it an alias. Try again!`);
-        if (bot.commands.has(command)) {
-            command = bot.commands.get(command);
-            var helpCommand = new Discord.RichEmbed();
-            helpCommand.setTitle(command.help.name)
+        var helpCommand = new Discord.RichEmbed();
+        helpCommand.setTitle(command.help.name)
             .addField('Description', `${command.help.description}`)
             .addField('Usage', `${bot.config.prefix}${command.help.usage}`)
             .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
-            if (command.conf.aliases != "") {
-                helpCommand.addField('Aliases', `${command.conf.aliases.join(', ')}`)
-            }
-            message.channel.send({embed: helpCommand});
-        };
+        if (command.conf.aliases != "") {
+            helpCommand.addField('Aliases', `${command.conf.aliases.join(', ')}`)
+        }
+        message.channel.send({
+            embed: helpCommand
+        });
     };
 };
 exports.conf = {
-    enabled: true, 
-    guildOnly: false, 
-    aliases: ['h', 'halp'], 
+    enabled: true,
+    guildOnly: false,
+    aliases: ['h', 'halp'],
     permLevel: 0
 };
 exports.help = {
-    name: 'help', 
-    description: 'Displays all the commands avaliable for your permission level', 
+    name: 'help',
+    description: 'Displays all the commands avaliable for your permission level',
     usage: 'help <command [optional]>'
 };
