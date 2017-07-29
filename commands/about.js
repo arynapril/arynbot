045a9c701b878ffabe9f5b51725a723373efacf0
arynapril <arynapril@gmail.com>
2017-07-29 08:39:39 -0400

    WHOOPS

--- commands/about.js
+++ commands/about.js
@@ -3,9 +3,9 @@ exports.run = (bot, message, args) => {
     uptime = bot.uptime;
     time = Math.floor(uptime/1000);
     if (time >= 3600) { 
-        var hours = Math.floor(x/3600); 
-        var minutes = Math.floor((x%3600)/60); 
-        var seconds = x%60%60;
+        var hours = Math.floor(time/3600); 
+        var minutes = Math.floor((time%3600)/60); 
+        var seconds = time%60%60;
         var timecode = `${hours} hours, ${minutes} minutes, and ${seconds} seconds.`
     } else if (time >= 60) {
         var minutes = Math.floor(time/60);
