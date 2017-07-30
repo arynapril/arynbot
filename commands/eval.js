a7e0d447c17bae8964871fffd4dfe219fe7664cc
arynapril <arynapril@gmail.com>
2017-07-30 12:01:44 -0400

    i like embeds okay

--- commands/eval.js
+++ commands/eval.js
@@ -1,13 +1,22 @@
 const Discord = require("discord.js");
 exports.run = async(bot, message, args, level) => {
+    message.delete();
     const code = args.join(" ");
+    var eval = new Discord.RichEmbed()
+        .setTitle('Evaluating code...')
+        .setTimestamp()
+        .setFooter('EVAL')
+        .addField('Input', code)
     try {
         const evaled = eval(code);
         const clean = await bot.clean(bot, evaled);
-        message.channel.send(`\`\`\`xl\n${evaled}\n\`\`\``);
+        eval.setColor([Math.floor(Math.random()*256),255,Math.floor(Math.random()*256)])
+            .addField('OUTPUT', clean)
     } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${await bot.clean(bot, err)}\n\`\`\``);
+        eval.setColor([255,Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+            .addField('ERROR', await bot.clean(bot, err))
     }
+    message.channel.send({embed: eval});
 };
 
 exports.conf = {
