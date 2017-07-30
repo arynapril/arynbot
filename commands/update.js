0dd21992189bfcd1a22b802127f09608fc06129f
arynapril <arynapril@gmail.com>
2017-07-30 13:28:08 -0400

    made it pretty

--- commands/update.js
+++ commands/update.js
@@ -1,14 +1,32 @@
 const child_process = require('child_process');
+const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
-    message.channel.send("Updating...").then(e => {
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
+    var updateEmbed = new Discord.RichEmbed()
+        .setColor([r, g, b])
+        .setDescription("Updating...")
+        .setTimestamp()
+        .setFooter('UPDATE')
+    message.channel.send({embed: updateEmbed}).then(e => {
         var update = child_process.execSync('git pull origin').toString();
-        message.channel.send(update);
-        e.edit("```" + update + "```");
         if (update.indexOf("Already up-to-date.") > -1) {
-            e.edit("There was nothing to update!");
+            var updateEmbed3 = new Discord.RichEmbed()
+                .setColor([r, g, b])
+                .setTitle("Updated!")
+                .setDescription("There was nothing to update!")
+                .setTimestamp()
+                .setFooter('Code is up to date!')
+            e.edit({embed: updateEmbed3});
         } else {
-            //process.exit();
-            e.channel.send("New code successfully pulled!\nRestarting...");
+            var updateEmbed2 = new Discord.RichEmbed()
+                .setColor([r, g, b])
+                .setTitle("Updated!")
+                .setDescription(update)
+                .setTimestamp()
+                .setFooter('New code successfully pulled! Restarting...')
+            e.edit({embed: updateEmbed2});
         }
     })
 };
