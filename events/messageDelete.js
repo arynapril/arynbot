7b3f5e1a6726ce8f7f999cbd757373c362573809
arynapril <arynapril@gmail.com>
2017-07-30 20:54:30 -0400

    logs deleted messages :P

---
+++ events/messageDelete.js
@@ -0,0 +1,15 @@
+module.exports = (bot, message) => {
+    const Discord = require('discord.js');
+    var logs = message.guild.channels.find('name', 'logs');
+    var dlt = new Discord.RichEmbed()
+        if (message.member.nickname == null) {
+            dlt.addField('User',`${message.author.username}`, true)
+        } else {
+            dlt.addField('User',`${message.member.nickname} (${message.author.username})`, true);
+        };
+        dlt.addField('Channel', message.channel.name, true)
+        .addField('Message', message)
+        .setTimestamp()
+        .setColor([255,Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
+    logs.send({embed: dlt});
+};
