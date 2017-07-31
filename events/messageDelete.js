11fe950404fc1bbae14212729e4aad99bcb5a846
arynapril <arynapril@gmail.com>
2017-07-31 09:24:20 -0400

    whoops

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,6 +1,7 @@
 module.exports = (bot, message) => {
     const Discord = require('discord.js');
     var logs = message.guild.channels.find('name', 'logs');
+    if (!logs) return;
     var dlt = new Discord.RichEmbed()
         if (message.member.nickname == null) {
             dlt.addField('User',`${message.author.username}`, true)
