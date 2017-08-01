6217f1a971987558c2844ec71847553e94eb10b4
arynapril <arynapril@gmail.com>
2017-07-31 22:54:55 -0400

    delete log has image support now

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,14 +3,23 @@ module.exports = (bot, message) => {
     var logs = message.guild.channels.find('name', 'logs');
     if (!logs) return;
     var dlt = new Discord.RichEmbed()
+        .setTitle('Message Deleted')
         if (message.member.nickname == null) {
             dlt.addField('User',`${message.author.username}`, true)
         } else {
             dlt.addField('User',`${message.member.nickname} (${message.author.username})`, true);
         };
         dlt.addField('Channel', message.channel.name, true)
-        .addField('Message', message)
-        .setTimestamp()
+        if (msg.attachments.size==0) {
+            dlt.addField('Message', `${message}`)
+        } else {
+            pictures = message.attachments.array();
+            if (message != "") {
+                dlt.addField('Message', `${message}`)
+            }
+            dlt.setImage(pictures[0].url)
+        }
+        dlt.setTimestamp()
         .setColor([255,Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
     logs.send({embed: dlt});
 };
