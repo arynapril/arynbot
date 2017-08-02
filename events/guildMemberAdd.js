33e72cccd420c1dc5d1290def13653439463439f
arynapril <arynapril@gmail.com>
2017-08-02 01:53:09 -0400

    this might work? who knows

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,9 @@
 module.exports = (bot, member) => {
-    member.guild.defaultChannel.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
+    var logs = member.guild.channels.find('name', 'logs');
+    if (member.guild.id == '317696745683550208') {
+        logs = member.guild.channels.find('name', 'general');
+    }
+    if (!logs) return;
+    logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,9 @@
 module.exports = (bot, member) => {
+    var logs = member.guild.channels.find('name', 'logs');
+    if (member.guild.id == '317696745683550208') {
+        logs = member.guild.channels.find('name', 'general');
+    }
+    if (!logs) return;
     member.guild.defaultChannel.send(`${member.user.username} has left the server! :cry:`);
     bot.log("log", `${member.guild.name} lost a member - ${member.user.username} (${member.id})`, 'MEMBR');
 };
