67336fc5ccdf4b1ae2ee1837671a63aa7abdb1c7
arynapril <arynapril@gmail.com>
2017-08-09 15:05:44 -0400

    test commit

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async bot => {
     await wait(1000);
     bot.log("log", `Ready to love ${bot.users.size} users in ${bot.guilds.size} servers!`, "READY");
-    bot.user.setGame("robots are gay");
+    bot.user.setGame("!help | robots are gay");
 }
