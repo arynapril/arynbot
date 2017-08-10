3027a6cd5fbd425eafa31de3f5764d0ba0389382
arynapril <arynapril@gmail.com>
2017-08-10 19:32:52 -0400

    derp

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = async(bot, message, args, level) => {
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! I took ${Math.round(bot.ping)} milliseconds to respond! The whole trip took ${m.createdTimestamp - message.createdTimestamp} milliseconds!`);
+    m.edit(`Pong! I took ${m.createdTimestamp - message.createdTimestamp} milliseconds to respond! Heartbeat is ${Math.round(bot.ping)} milliseconds!`);
 };
 
 exports.conf = {
