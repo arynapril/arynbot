f4653d3b3be1fa493479bbc934f3086e19d17b72
arynapril <arynapril@gmail.com>
2017-08-13 12:59:31 -0400

    minor logging edit

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,5 @@
 module.exports = (bot, member) => {
+    bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
     var logs = member.guild.channels.find('name', 'logs');
     if (member.guild.id == '317696745683550208') {
         logs = member.guild.channels.find('name', 'general');
@@ -6,5 +7,4 @@ module.exports = (bot, member) => {
     if (!logs) return;
     if (!bot.user.hasPermission("SEND_MESSAGES")) return;
     logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
-    bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
 };
