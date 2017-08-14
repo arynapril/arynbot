ee9e8fd2f29b1af6986ccd9f3128f10e70b32603
arynapril <arynapril@gmail.com>
2017-08-13 23:46:28 -0400

    minor logging edit

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,9 +1,10 @@
 module.exports = (bot, member) => {
+    bot.log("log", `${member.guild.name} lost a member - ${member.user.username} (${member.id})`, 'MEMBR');
     var logs = member.guild.channels.find('name', 'logs');
     if (member.guild.id == '317696745683550208') {
         logs = member.guild.channels.find('name', 'general');
     }
     if (!logs) return;
+    if (!bot.user.hasPermission("SEND_MESSAGES")) return;
     logs.send(`${member.user.username} has left the server! :cry:`);
-    bot.log("log", `${member.guild.name} lost a member - ${member.user.username} (${member.id})`, 'MEMBR');
 };
