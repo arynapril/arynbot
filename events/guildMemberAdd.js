3bfef54e12ecf88a609ad8ee02535464714693cf
arynapril <arynapril@gmail.com>
2017-08-18 15:45:56 -0400

    im dum

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,10 @@
 module.exports = (bot, member) => {
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
-    if(!message.channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
     var logs = member.guild.channels.find('name', 'logs');
     if (member.guild.id == '317696745683550208') {
         logs = member.guild.channels.find('name', 'general');
     }
     if (!logs) return;
+    if(!logs.permissionsFor(member.guild.me).has("SEND_MESSAGES")) return;
     logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
 };
