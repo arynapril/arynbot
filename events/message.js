109665b3cb23907286d6b88f8e2af4dbe3bad28c
arynapril <arynapril@gmail.com>
2017-08-30 19:52:19 -0400

    minor dad joke tweaks

--- events/message.js
+++ events/message.js
@@ -5,16 +5,20 @@ module.exports = (bot, message) => {
         return;
     };
     if(!message.channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
-    if (message.content.startsWith("im ") || message.content.startsWith("i'm ") || message.content.startsWith("I'm ") || message.content.startsWith("IM ") || message.content.startsWith("IM ") || message.content.startsWith("I am ")) {
+    if (message.content.toLowerCase.startsWith("im ") || message.content.toLowerCase.startsWith("i'm ") || message.content.toLowerCase.startsWith("i am ")) {
         if (message.author.id == '226999841358610432') {
             const args = message.content.split(/\s+/g);
             const cmd = bot.commands.get('love');
             cmd.run(bot, message, args);
         } else {
             if (message.guild.id == '292134570940301312' && message.channel.id !== '296497111090855936') return;
-            let args = message.cleanContent.split(" ").slice(1);
-            if (args.length > 5) return;
-            let joke = args.join(" ");
+            if (messae.content.toLowerCase.startsWith('i am ')) {
+                joke1 = message.cleanContent.split(" ").slice(2);
+            } else {
+                joke1 = message.cleanContent.split(" ").slice(1);
+            }
+            if (joke1.length > 5) return;
+            let joke = joke1.join(" ");
             message.channel.send(`Hi **${joke}**, I'm arynbot! :heart:`);
             bot.log("log", `${message.guild.name}/#${message.channel.name}: ${message.author.username} (${message.author.id}) made a dad joke: ${joke}`, "DAD  ");
         }
