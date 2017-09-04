10cf2c394142f6684e8c0d6498bf9ae1cd4f8c39
arynapril <arynapril@gmail.com>
2017-09-04 16:45:13 -0400

    tweaks to sql

--- commands/settings.js
+++ commands/settings.js
@@ -3,12 +3,13 @@ const db = new sqlite3.Database('../servers.sqlite');
 exports.run = (bot, message, args, level) => {
     	if (!message.member.hasPermission('MANAGE_GUILD')) return msg.reply("you do not have permission to manage this server's setings!");
         if (args[0] == 'dadjokes') {
-            db.all(`SELECT * FROM servers WHERE id = "${message.guild.id}"`, function (results, err) {
-				if (err || !results)
+            db.all(`SELECT name name FROM servers WHERE id = "${message.guild.id}"`, 1, (err, row) => {
+				if (err) {
 					bot.log('log', err, 'ERROR');
-				else
-					dadJokesEnabled = results.rows[0].dadJokes;
+                } else {
+					dadJokesEnabled = row.name;
                     message.channel.send(`dadjokes current setting: ${dadJokesEnabled}`);
+                };
 			});
             //if (!args[1]) return 
         }
