164bcaf062a30a47eeb1c8b6b7639f954d76baa6
arynapril <arynapril@gmail.com>
2017-09-04 13:42:11 -0400

    maybe?

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,4 @@ module.exports = async bot => {
     await wait(1000);
     bot.syncServers();
     bot.log("log", `Ready to love ${bot.users.size} users in ${bot.guilds.size} servers!`, "READY");
-    bot.user.setGame("!help | robots are gay");
 }
