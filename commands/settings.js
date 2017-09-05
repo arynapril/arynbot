3d60d358f6e0b363910600802719f09146478e3a
arynapril <arynapril@gmail.com>
2017-09-05 13:16:38 -0400

    derp

--- commands/settings.js
+++ commands/settings.js
@@ -3,7 +3,7 @@ const db = new sqlite3.Database('../servers.sqlite');
 exports.run = (bot, message, args, level) => {
     	if (!message.member.hasPermission('MANAGE_GUILD')) return msg.reply("you do not have permission to manage this server's setings!");
         if (args[0] == 'prefix') {
-            bot.getSetting(prefix, message).then(prefix => {
+            bot.getSetting('prefix', message).then(prefix => {
                 message.channel.send(prefix)
             })
             //if (!args[1]) return 
