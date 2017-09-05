0447a01decab32f4702d9d08d955209ece11ed05
arynapril <arynapril@gmail.com>
2017-09-04 20:44:21 -0400

    im stupid

--- commands/settings.js
+++ commands/settings.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('../servers.sqlite');
 exports.run = (bot, message, args, level) => {
     	if (!message.member.hasPermission('MANAGE_GUILD')) return msg.reply("you do not have permission to manage this server's setings!");
-        if (args[0] == 'dadjokes') {
+        if (args[0] == 'dadJokes') {
             message.channel.send(bot.getSetting('dadJokes', message));
             //if (!args[1]) return 
         }
