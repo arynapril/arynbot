763bd69dd7e244bf01f94342deceefa4a49cd72f
arynapril <arynapril@gmail.com>
2017-09-05 23:12:33 -0400

    IM DUMB

--- commands/settings.js
+++ commands/settings.js
@@ -3,7 +3,7 @@ const db = new sqlite3.Database('../servers.sqlite');
 exports.run = (bot, message, args, level) => {
     settingsArray = ['dadJokes', 'dadJokesJail', 'hallOfFameEnabled', 'hallOfFameEmote', 'hallOfFameLimit', 'hallOfFameChannel']
     	if (!message.member.hasPermission('MANAGE_GUILD')) return msg.reply("you do not have permission to manage this server's setings!");
-        if (args[0].indexOf(settingsArray) != 0 && !args[1]) {
+        if (args[0].indexOf(settingsArray) != 0) {
             if (!args[1]) {
                 bot.getSetting(args[0], message).then(setting => {
                     message.channel.send(setting)
