54a71cb0437bed01cab952318c85da3b5161638b
arynapril <arynapril@gmail.com>
2017-09-05 23:21:01 -0400

    wow this will definitly not work

--- functions.js
+++ functions.js
@@ -59,8 +59,17 @@ module.exports = (bot) => {
         return roles;
     };
     bot.setSetting = function(setting, newSetting, message) {
-        db.run(`UPDATE servers SET ${setting} = "${newSetting}" WHERE id = "${message.guild.id}"`);
-        return newSetting;
+        return new Promise (
+			function(resolve, reject) {
+				db.run(`UPDATE servers SET ${setting} = "${newSetting}" WHERE id = "${message.guild.id}"`), function (err, rows) {
+                    if (err || !rows[0])
+                        reject(err);
+                    else
+                        resolve(rows[0][setting])
+                }});
+            }
+        	return newSetting;
+		)
     };
     bot.permLevel = function(message) {
         if (message.author.id == bot.config.owner)
