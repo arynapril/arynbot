44883315623283abc3e3a689da9992e11e2e7171
arynapril <arynapril@gmail.com>
2017-09-07 21:39:47 -0400

    hm

--- commands/settings.js
+++ commands/settings.js
@@ -5,15 +5,15 @@ exports.run = (bot, message, args, level) => {
     booleanArray = ['dadJokesEnabled', 'hallOfFameEnabled', 'welcomeMessagesEnabled'];
     channelArray = ['dadJokesJail', 'hallOfFameChannel', 'welcomeMessageChannel'];
     	if (!message.member.hasPermission('MANAGE_GUILD')) return message.reply("you do not have permission to manage this server's setings!");
-        if (args[0].indexOf(settingsArray) != 0) {
+        if (args[0].indexOf(settingsArray) != -1) {
             if (!args[1]) {
                 bot.getSetting(args[0], message).then(setting => {
                     message.channel.send(`The current **${args[0]}** setting is **${setting}**`);
                 })
             } else {
                 if (args[2]) return message.channel.send('Please enter only one value!')
-                if (args[0].indexOf(booleanArray) != 0 && (args[1] !== 1 || args[1] !== 0)) return message.channel.send(`The ${args[0]} value must be be either a 0 or a 1! Please try again!`);
-                if (args[0].indexOf(channelArray) != 0 && !message.mentions.channels.array()[0]) return message.channel.send(`The ${args[0]} value must be a channel mention! Please try again!`);
+                if (args[0].indexOf(booleanArray) != -1 && (args[1] !== 1 || args[1] !== 0)) return message.channel.send(`The ${args[0]} value must be be either a 0 or a 1! Please try again!`);
+                if (args[0].indexOf(channelArray) != -1 && !message.mentions.channels.array()[0]) return message.channel.send(`The ${args[0]} value must be a channel mention! Please try again!`);
                 if (args[0] == 'hallOfFameLimit' && !isNaN(args[1])) return message.channel.send(`The ${args[0]} value must be a whole number! Please try again!`);
                 //add check for the hallOfFameEmoji setting
                 bot.setSetting(args[0], args[1], message).then(setting => {
