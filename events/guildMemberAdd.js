34afc645e1dc60f184afdf7340d34254bf20e485
arynapril <arynapril@gmail.com>
2017-09-08 00:16:41 -0400

    a general test

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,14 +1,13 @@
 module.exports = (bot, member) => {
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
-    /*var logs = member.guild.channels.find('name', 'logs');
+    var logs = member.guild.channels.find('name', 'logs');
     if (member.guild.id == '317696745683550208') {
         logs = member.guild.channels.find('name', 'general');
     }
     if (!logs) return;
-    if(!logs.permissionsFor(member.guild.me).has("SEND_MESSAGES")) return;
-    logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);*/
     if(bot.getSetting('welcomeMessagesEnabled', member.guild) == '0') return;
-    welcome = member.guild.channels.find('name', bot.getSetting('welcomeMessagesChannel', member.guild));
-    if(!welcome) return;
-    welcome.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
+    //welcome = member.guild.channels.find('name', bot.getSetting('welcomeMessagesChannel', member.guild));
+    //if(!welcome) return;
+    //welcome.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
+    logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile: ${bot.getSetting('welcomeMessagesChannel', member.guild)}`);
 };
