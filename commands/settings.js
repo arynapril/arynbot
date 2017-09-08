cd625737bc0cb882572df3fc0b7ffbbd25129b16
arynapril <arynapril@gmail.com>
2017-09-07 22:17:26 -0400

    im stupid

--- commands/settings.js
+++ commands/settings.js
@@ -14,7 +14,7 @@ exports.run = (bot, message, args, level) => {
                 if (args[2]) return message.channel.send('Please enter only one value!')
                 if (booleanArray.indexOf(args[0]) != -1 && (args[1] !== 1 || args[1] !== 0)) return message.channel.send(`The ${args[0]} value must be be either a 0 or a 1! Please try again!`);
                 if (channelArray.indexOf(args[0]) != -1 && !message.mentions.channels.array()[0]) return message.channel.send(`The ${args[0]} value must be a channel mention! Please try again!`);
-                if (args[0] == 'hallOfFameLimit' && !isNaN(args[1])) return message.channel.send(`The ${args[0]} value must be a whole number! Please try again!`);
+                if (args[0] == 'hallOfFameLimit' && isNaN(args[1])) return message.channel.send(`The ${args[0]} value must be a whole number! Please try again!`);
                 //add check for the hallOfFameEmoji setting
                 bot.setSetting(args[0], args[1], message).then(setting => {
                     message.channel.send(`**${args[0]}** setting successfully changed to **${setting}**`);
