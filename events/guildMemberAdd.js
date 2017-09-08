989e63f90e13fbe10a0e9dacba8ae1a8180217e4
arynapril <arynapril@gmail.com>
2017-09-07 22:45:27 -0400

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,8 +1,8 @@
 module.exports = (bot, member) => {
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
-    bot.getSetting('welcomeMessagesEnabled', member.guild).then(setting => {
-        if(!setting) return;
+   // bot.getSetting('welcomeMessagesEnabled', member.guild).then(setting => {
+   //     if(!setting) return;
         //member.guild.channels.get(setting.mentions.channels.array[0].id).send('test');
-    })
+   // })
     //logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
 };
