6610d96a3927755455c8085fe898caef8eb5b99d
arynapril <arynapril@gmail.com>
2017-09-07 22:36:00 -0400

    a test fix

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ module.exports = (bot, member) => {
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
     bot.getSetting('welcomeMessagesEnabled', member.guild).then(setting => {
         if(!setting) return;
-        member.guild.channels.get(setting.mentions.channels.array[0].id).send('test');
+        //member.guild.channels.get(setting.mentions.channels.array[0].id).send('test');
     })
     //logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
 };
