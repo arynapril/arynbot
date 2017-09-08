4020362c526325c3dc5cdd14f52498440c862c18
arynapril <arynapril@gmail.com>
2017-09-08 14:42:24 -0400

    lets try this

--- events/message.js
+++ events/message.js
@@ -5,6 +5,23 @@ module.exports = (bot, message) => {
 		console.log("[DM] " + message.channel.recipient.username + " -> " + bot.user.username + " | " + message.content)
     if(!message.channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
     x = message.content.toLowerCase();
+	bot.dadJoke = function(message) {
+		if (message.author.id == '226999841358610432') {
+            const args = message.content.split(/\s+/g);
+            const cmd = bot.commands.get('love');
+            cmd.run(bot, message, args);
+        } else {
+            if (x.startsWith('i am ')) {
+                joke1 = message.cleanContent.split(" ").slice(2);
+            } else {
+                joke1 = message.cleanContent.split(" ").slice(1);
+            }
+            if (joke1.length > 5) return;
+            let joke = joke1.join(" ");
+            message.channel.send(`Hi **${joke}**, I'm arynbot! :heart:`);
+            bot.log("log", `${message.guild.name}/#${message.channel.name}: ${message.author.username} (${message.author.id}) made a dad joke: ${joke}`, "DAD  ");
+        };
+	};
     if (x.startsWith("im ") || x.startsWith("i'm ") || x.startsWith("i am ")) {
         bot.getSetting('dadJokesEnabled', message.guild).then(setting => {
             if (setting == '0') return;
