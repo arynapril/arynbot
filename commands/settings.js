fada2783348ba463c72cb36946c051d7f354377f
arynapril <arynapril@gmail.com>
2017-09-08 14:13:00 -0400

    this probably isnt how you're supposed to do this

--- commands/settings.js
+++ commands/settings.js
@@ -24,7 +24,7 @@ exports.run = (bot, message, args, level) => {
                     }
                     if (!channelFound) return message.channel.send(`The ${args[0]} value must be the name of an channel on this server! Please try again!`)
                 };
-                if (args[0] == 'hallOfFameLimit' && !isNaN(args[1])) return message.channel.send(`The ${args[0]} value must be a whole number! Please try again!`);
+                if (args[0] == 'hallOfFameLimit' && !isNaN(parseInt(args[1]))) return message.channel.send(`The ${args[0]} value must be a whole number! Please try again!`);
                 if (args[0] == 'hallOfFameEmote') {
                     found = false;
                     emoji = message.guild.emojis.array();
