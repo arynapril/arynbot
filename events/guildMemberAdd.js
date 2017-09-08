346b527500d0a5fe2517c6b6aa8ee05208f56d30
arynapril <arynapril@gmail.com>
2017-09-08 00:27:19 -0400

    oh boy now to test if enabling and disabling works

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,15 +1,10 @@
 module.exports = (bot, member) => {
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
-    //var logs = member.guild.channels.find('name', 'logs');
-    //if (member.guild.id == '317696745683550208') {
-    //    logs = member.guild.channels.find('name', 'general');
-    //}
-    //if (!logs) return;
-    //if(bot.getSetting('welcomeMessagesEnabled', member.guild) == '0') return;
-    bot.getSetting('welcomeMessagesChannel', member.guild).then(setting => {
-        welcome = member.guild.channels.find('name', setting);
-        welcome.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
+    bot.getSetting('welcomeMessagesEnabled', member.guild).then(enabled => {
+        if (enabled == '0') return;
+        bot.getSetting('welcomeMessagesChannel', member.guild).then(setting => {
+            welcome = member.guild.channels.find('name', setting);
+            welcome.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
+        })
     })
-    //if(!welcome) return;
-    //logs.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
 };
