f1eeffddb49bd1cb2f95bed30e6bf332c5a39e13
arynapril <arynapril@gmail.com>
2017-09-08 14:44:04 -0400

    augh i didnt want to do it this way

--- functions.js
+++ functions.js
@@ -147,23 +147,6 @@ module.exports = (bot) => {
             }
         })
     }
-	bot.dadJoke = function(message) {
-		if (message.author.id == '226999841358610432') {
-            const args = message.content.split(/\s+/g);
-            const cmd = bot.commands.get('love');
-            cmd.run(bot, message, args);
-        } else {
-            if (x.startsWith('i am ')) {
-                joke1 = message.cleanContent.split(" ").slice(2);
-            } else {
-                joke1 = message.cleanContent.split(" ").slice(1);
-            }
-            if (joke1.length > 5) return;
-            let joke = joke1.join(" ");
-            message.channel.send(`Hi **${joke}**, I'm arynbot! :heart:`);
-            bot.log("log", `${message.guild.name}/#${message.channel.name}: ${message.author.username} (${message.author.id}) made a dad joke: ${joke}`, "DAD  ");
-        };
-	};
     bot.getSetting = function(input, g) {
         return new Promise(
             function(resolve, reject) {
