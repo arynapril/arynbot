7711b2144f033679964c7571c861e8e4f6f7a257
arynapril <arynapril@gmail.com>
2017-09-25 19:37:48 -0400

    updated color command

--- commands/color.js
+++ commands/color.js
@@ -3,7 +3,7 @@ exports.run = (bot, message, args, level) => {
     var color = new Discord.RichEmbed()
         .setColor('RANDOM')
     if ((typeof args[0] === "string") && args[0].length === 6 && !isNaN(parseInt(args[0], 16))) {
-        hexColor = `#${args[0]}`;
+        hexColor = args[0].toLowerCase();
         var hex = parseInt(args[0], 16);
         var r = (hex >> 16) & 255;
         var g = (hex >> 8) & 255;
@@ -13,19 +13,21 @@ exports.run = (bot, message, args, level) => {
     } else if (args[0] < 256 && args [1] < 256 && args[2] < 256 && !args[3]) {
         rgb = args.join(', ');
         var rgbCode = args[2] | (args[1] << 8) | (args[0] << 16);
-        hexColor = '#' + (0x1000000 + rgbCode).toString(16).slice(1);
+        hexColor = (0x1000000 + rgbCode).toString(16).slice(1).toLowerCase();
         color.setColor(hexColor) 
     } else if (!args[0]) {
-        hexColor = '#' + parseInt(color.color).toString(16);
+        hexColor = parseInt(color.color).toString(16).toLowerCase();
         var hex = parseInt(color.color, 16);
         var r = (hex >> 16) & 255;
         var g = (hex >> 8) & 255;
         var b = hex & 255;
         rgb = r + ", " + g + ", " + b;
     }
-    color.addField('Hex', hexColor)
-         .addField('RGB', rgb)
-         .addField('Decimal', color.color)
+    color.addField('Hex', `#${hexColor}`)
+        .addField('RGB', rgb)
+        .addField('Decimal', color.color)
+        .addField('More Information', `[Here](http://www.colorhexa.com/${hexColor})`)
+        .setThumbnail(`http://www.colorhexa.com/${hexColor}.png`)
     message.channel.send({embed: color})
 };
 
