b2117981e6c71ed6c9be185e99c47b9b0f155ac0
arynapril <arynapril@gmail.com>
2017-09-25 19:37:59 -0400

    removed a line that wasnt doing anything

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,7 +1,6 @@
 module.exports = (bot, oldMember, newMember) => {
     let role = newMember.guild.roles.find("name", 'voice');
     if (!role) return;
-    //if(!newMember.guild.permissionsFor(newMember.guild.me).has('MANAGE_ROLES')) return;
     if (newMember.voiceChannel) {
         newMember.addRole(role);
     } else {
