513275bd999906b3e861e3fdeff93453e28dfdcc
arynapril <arynapril@gmail.com>
2017-09-28 20:39:58 -0400

    wont crash when dmd anymore

--- events/message.js
+++ events/message.js
@@ -1,8 +1,8 @@
 module.exports = (bot, message) => {
     if (message.channel.type === "dm" && message.author.id == bot.user.id)
-        console.log("[DM] " + bot.user.username + " -> " + message.channel.recipient.username + " | " + message.content)
+        return console.log("[DM] " + bot.user.username + " -> " + message.channel.recipient.username + " | " + message.content);
     else if (message.channel.type === "dm" && message.author.id != bot.user.id)
-        console.log("[DM] " + message.channel.recipient.username + " -> " + bot.user.username + " | " + message.content)
+        return console.log("[DM] " + message.channel.recipient.username + " -> " + bot.user.username + " | " + message.content);
     if (!message.channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
     x = message.content.toLowerCase();
     if (x.startsWith("im ") || x.startsWith("i'm ") || x.startsWith("i am ")) {
