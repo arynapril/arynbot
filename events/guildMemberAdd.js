ec19dddad2bb2320c0fd4cbf2512b4dad38f7eb4
arynapril <arynapril@gmail.com>
2017-10-01 11:36:59 -0400

    michael said this should work

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,8 +2,8 @@ module.exports = async (bot, member) => {
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
     enabled = await bot.getSetting('welcomeMessagesEnabled', member.guild);
     if (enabled == '0')
-    chan = await bot.getSetting('welcomeMessagesChannel', member.guild);
-    welcome = member.guild.channels.find('name', chan);
+    channelName = await bot.getSetting('welcomeMessagesChannel', member.guild);
+    welcome = member.guild.channels.find('name', channelName);
     if (!welcome) return;
     welcome.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
 };
