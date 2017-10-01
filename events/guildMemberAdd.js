2594c763fddca83f770c831c0957547b18c8608a
arynapril <arynapril@gmail.com>
2017-10-01 11:44:20 -0400

    wherein michael is a heckin liar but also i found the error

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
-module.exports = async (bot, member) => {
+module.exports = async(bot, member) => {
     bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
     enabled = await bot.getSetting('welcomeMessagesEnabled', member.guild);
-    if (enabled == '0')
+    if (enabled == '0') return;
     channelName = await bot.getSetting('welcomeMessagesChannel', member.guild);
     welcome = member.guild.channels.find('name', channelName);
     if (!welcome) return;
