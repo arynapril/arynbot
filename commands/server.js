58be37dea49125c2f0f08bc7f395b7bcf0cedc8a
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:41:13 -0400

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,31 +1,33 @@
 exports.run = (bot, message, args, level) => {
-    const Discord = require('discord.js');
-    var server = message.guild;
-    var serverEmbed = new Discord.RichEmbed()
-        .setTitle(`Server stats for ${server.name}`)
-        .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
-        .addField('Name', server.name)
-        .addField('Owner', server.owner.displayName)
-        .addField('Created', server.createdAt, true)
-        .addField('Channels', server.channels.size, true)
-        .addField('Members', server.members.size, true)
-        .addField('Roles', server.roles.size, true)
-        .setThumbnail(server.iconURL)
-        .setFooter(`${server.name} STATS`)
-        .setTimestamp()
-    message.channel.send({embed: serverEmbed});
+	const Discord = require('discord.js');
+	var server = message.guild;
+	var serverEmbed = new Discord.RichEmbed()
+		.setTitle(`Server stats for ${server.name}`)
+		.setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
+		.addField('Name', server.name)
+		.addField('Owner', server.owner.displayName)
+		.addField('Created', server.createdAt, true)
+		.addField('Channels', server.channels.size, true)
+		.addField('Members', server.members.size, true)
+		.addField('Roles', server.roles.size, true)
+		.setThumbnail(server.iconURL)
+		.setFooter(`${server.name} STATS`)
+		.setTimestamp()
+	message.channel.send({
+		embed: serverEmbed
+	});
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    botPerms: [],
-    memberPerms: []
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	botPerms: [],
+	memberPerms: []
 };
 
 exports.help = {
-    name: 'server',
-    description: 'Displays a few stats about the server!',
-    usage: 'server'
+	name: 'server',
+	description: 'Displays a few stats about the server!',
+	usage: 'server'
 };
