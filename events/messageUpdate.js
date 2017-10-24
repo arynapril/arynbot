c1c7609a0d598c4fb7d17c71112b0e849290b6f4
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:45:07 -0400

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,39 +1,41 @@
 module.exports = (bot, oldMessage, newMessage) => {
-    const Discord = require('discord.js');
-    var logs = oldMessage.guild.channels.find('name', 'logs');
-    if (!logs) return;
-    if(!logs.permissionsFor(oldMessage.guild.me).has("SEND_MESSAGES")) return;
-    if (!oldMessage || !newMessage) return;
-    if (oldMessage == "" || newMessage == "") return; 
-    if (oldMessage == newMessage) return;
-    if (oldMessage.author.bot) return;
-    var update = new Discord.RichEmbed()
-        .setTitle('Message Updated')
-        if (oldMessage.member.nickname == null) {
-            update.addField('User',`${oldMessage.author.username}`, true)
-        } else {
-            update.addField('User',`${oldMessage.member.nickname} (${oldMessage.author.username})`, true);
-        };
-        update.addField('Channel', oldMessage.channel.name, true)
-        if (oldMessage.attachments.size==0) {
-            update.addField('Old Message', `${oldMessage}`)
-        } else {
-            var pictures = oldMessage.attachments.array();
-            if (oldMessage != "") {
-                update.addField('Old Message', `${oldMessage}`)
-            }
-            update.addField('Old Image', pictures[0].url);
-        };
-        if (newMessage.attachments.size==0) {
-            update.addField('New Message', `${newMessage}`)
-        } else {
-            var newPictures = newMessage.attachments.array();
-            if (newMessage != "") {
-                update.addField('New Message', `${newMessage}`)
-            }
-            update.addField('New Image', newPictures[0].url);
-        };
-        update.setTimestamp()
-        .setColor('#FFC400')
-    logs.send({embed: update});
+	const Discord = require('discord.js');
+	var logs = oldMessage.guild.channels.find('name', 'logs');
+	if (!logs) return;
+	if (!logs.permissionsFor(oldMessage.guild.me).has("SEND_MESSAGES")) return;
+	if (!oldMessage || !newMessage) return;
+	if (oldMessage == "" || newMessage == "") return;
+	if (oldMessage == newMessage) return;
+	if (oldMessage.author.bot) return;
+	var update = new Discord.RichEmbed()
+		.setTitle('Message Updated')
+	if (oldMessage.member.nickname == null) {
+		update.addField('User', `${oldMessage.author.username}`, true)
+	} else {
+		update.addField('User', `${oldMessage.member.nickname} (${oldMessage.author.username})`, true);
+	};
+	update.addField('Channel', oldMessage.channel.name, true)
+	if (oldMessage.attachments.size == 0) {
+		update.addField('Old Message', `${oldMessage}`)
+	} else {
+		var pictures = oldMessage.attachments.array();
+		if (oldMessage != "") {
+			update.addField('Old Message', `${oldMessage}`)
+		}
+		update.addField('Old Image', pictures[0].url);
+	};
+	if (newMessage.attachments.size == 0) {
+		update.addField('New Message', `${newMessage}`)
+	} else {
+		var newPictures = newMessage.attachments.array();
+		if (newMessage != "") {
+			update.addField('New Message', `${newMessage}`)
+		}
+		update.addField('New Image', newPictures[0].url);
+	};
+	update.setTimestamp()
+		.setColor('#FFC400')
+	logs.send({
+		embed: update
+	});
 };
