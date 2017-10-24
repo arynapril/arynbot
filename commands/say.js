d51b81deacc0bf8ab9adc48885cfa0bba4502b68
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:40:52 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,18 +1,18 @@
 exports.run = (bot, message, args, level) => {
-    message.delete();
-    message.channel.send(args.join(" "));
+	message.delete();
+	message.channel.send(args.join(" "));
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    botPerms: ['MANAGE_MESSAGES'],
-    memberPerms: ['MANAGE_MESSAGES']
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	botPerms: ['MANAGE_MESSAGES'],
+	memberPerms: ['MANAGE_MESSAGES']
 };
 
 exports.help = {
-    name: 'say',
-    description: 'Says something',
-    usage: 'say <what you want her to say'
+	name: 'say',
+	description: 'Says something',
+	usage: 'say <what you want her to say'
 };
