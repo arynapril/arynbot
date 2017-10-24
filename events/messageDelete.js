49003ae31d3b3b671e12513db9952badb220944c
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:44:37 -0400

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,27 +1,29 @@
 module.exports = (bot, message) => {
-    const Discord = require('discord.js');
-    var logs = message.guild.channels.find('name', 'logs');
-    if (!logs) return;
-    if(!logs.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
-    if (message.author.bot) return;
-    var dlt = new Discord.RichEmbed()
-        .setTitle('Message Deleted')
-        if (message.member.nickname == null) {
-            dlt.addField('User',`${message.author.username}`, true)
-        } else {
-            dlt.addField('User',`${message.member.nickname} (${message.author.username})`, true);
-        };
-        dlt.addField('Channel', message.channel.name, true)
-        if (message.attachments.size==0) {
-            dlt.addField('Message', `${message}`)
-        } else {
-            pictures = message.attachments.array();
-            if (message != "") {
-                dlt.addField('Message', `${message}`)
-            }
-            dlt.setImage(pictures[0].url)
-        }
-        dlt.setTimestamp()
-        .setColor('#E53935')
-    logs.send({embed: dlt});
+	const Discord = require('discord.js');
+	var logs = message.guild.channels.find('name', 'logs');
+	if (!logs) return;
+	if (!logs.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return;
+	if (message.author.bot) return;
+	var dlt = new Discord.RichEmbed()
+		.setTitle('Message Deleted')
+	if (message.member.nickname == null) {
+		dlt.addField('User', `${message.author.username}`, true)
+	} else {
+		dlt.addField('User', `${message.member.nickname} (${message.author.username})`, true);
+	};
+	dlt.addField('Channel', message.channel.name, true)
+	if (message.attachments.size == 0) {
+		dlt.addField('Message', `${message}`)
+	} else {
+		pictures = message.attachments.array();
+		if (message != "") {
+			dlt.addField('Message', `${message}`)
+		}
+		dlt.setImage(pictures[0].url)
+	}
+	dlt.setTimestamp()
+		.setColor('#E53935')
+	logs.send({
+		embed: dlt
+	});
 };
