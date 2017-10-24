d5d3510b8d6cb5c4e28d8ae24d685a833545e377
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:43:12 -0400

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
-module.exports = async(bot, member) => {
-    bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
-    enabled = await bot.getSetting('welcomeMessagesEnabled', member.guild);
-    if (enabled == '0') return;
-    channelName = await bot.getSetting('welcomeMessagesChannel', member.guild);
-    welcome = member.guild.channels.find('name', channelName);
-    if (!welcome) return;
-    welcome.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
+module.exports = async (bot, member) => {
+	bot.log("log", `${member.guild.name} got a new member - ${member.user.username} (${member.id})`, 'MEMBR');
+	enabled = await bot.getSetting('welcomeMessagesEnabled', member.guild);
+	if (enabled == '0') return;
+	channelName = await bot.getSetting('welcomeMessagesChannel', member.guild);
+	welcome = member.guild.channels.find('name', channelName);
+	if (!welcome) return;
+	welcome.send(`Hi ${member}! Welcome to ${member.guild.name}! :smile:`);
 };
