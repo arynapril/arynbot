f5b9b1c9be94184e24cf2e56056887e566f0dfda
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:28:07 -0400

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,42 +1,44 @@
 exports.run = (bot, message, args) => {
-    const Discord = require('discord.js');
-    uptime = bot.uptime;
-    time = Math.floor(uptime/1000);
-    if (time >= 3600) { 
-        var hours = Math.floor(time/3600); 
-        var minutes = Math.floor((time%3600)/60); 
-        var seconds = time%60%60;
-        var timecode = `${hours} hours, ${minutes} minutes, and ${seconds} seconds.`
-    } else if (time >= 60) {
-        var minutes = Math.floor(time/60);
-        var seconds = time%60;
-        var timecode = `${minutes} minutes and ${seconds} seconds`;
-    } else {
-        var timecode = `${time} seconds`;
-    }
-    about = new Discord.RichEmbed()
-    .setTitle("About Arynbot")
-    .setDescription("Just a simple bot who is full of love!")
-    .setColor([Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256)])
-    .addField('Botmom', 'arynapril#8541')
-    .addField('Users', bot.users.size, true)
-    .addField('Servers', bot.guilds.size, true)
-    .addField('Channels', bot.channels.size, true)
-    .addField('Uptime', timecode, true)
-    .setThumbnail(bot.user.avatarURL)
-    message.channel.send({embed: about});
+	const Discord = require('discord.js');
+	uptime = bot.uptime;
+	time = Math.floor(uptime / 1000);
+	if (time >= 3600) {
+		var hours = Math.floor(time / 3600);
+		var minutes = Math.floor((time % 3600) / 60);
+		var seconds = time % 60 % 60;
+		var timecode = `${hours} hours, ${minutes} minutes, and ${seconds} seconds.`
+	} else if (time >= 60) {
+		var minutes = Math.floor(time / 60);
+		var seconds = time % 60;
+		var timecode = `${minutes} minutes and ${seconds} seconds`;
+	} else {
+		var timecode = `${time} seconds`;
+	}
+	about = new Discord.RichEmbed()
+		.setTitle("About Arynbot")
+		.setDescription("Just a simple bot who is full of love!")
+		.setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
+		.addField('Botmom', 'arynapril#8541')
+		.addField('Users', bot.users.size, true)
+		.addField('Servers', bot.guilds.size, true)
+		.addField('Channels', bot.channels.size, true)
+		.addField('Uptime', timecode, true)
+		.setThumbnail(bot.user.avatarURL)
+	message.channel.send({
+		embed: about
+	});
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['stats'],
-    botPerms: [],
-    memberPerms: []
+	enabled: true,
+	guildOnly: false,
+	aliases: ['stats'],
+	botPerms: [],
+	memberPerms: []
 };
 
 exports.help = {
-    name: 'about',
-    description: 'display a few statistics for the bot',
-    usage: 'about'
+	name: 'about',
+	description: 'display a few statistics for the bot',
+	usage: 'about'
 };
