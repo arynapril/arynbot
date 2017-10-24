aefa46900ab7a42dc0f8bd0684b0524316e8709b
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:42:14 -0400

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,36 +1,40 @@
 exports.run = (bot, message, args, level) => {
-    const Discord = require("discord.js");
-    var mutee = message.mentions.users.array();
-    for(var k = 0; k < mutee.length; k++) {
-        var member = message.guild.members.get(mutee[k].id);
-        var user = bot.users.get(mutee[k].id);
-        var guild = message.guild;
-        var channels = message.guild.channels.array();
-        for(var i=0; i < channels.length; i++) {
-            if(channels[i].type == 'text')
-                channels[i].overwritePermissions(member, {SEND_MESSAGES: null})
-        };
-        var unmute = new Discord.RichEmbed();
-        unmute.setTitle('User was unmuted.')
-        .addField('User', user)
-        .addField('Unmuted by', message.author)
-        .setFooter("UNMUTED")
-        .setTimestamp()
-        .setColor([Math.floor(Math.random()*256),255,Math.floor(Math.random()*256)])
-        message.channel.send({embed: unmute})
-    };
+	const Discord = require("discord.js");
+	var mutee = message.mentions.users.array();
+	for (var k = 0; k < mutee.length; k++) {
+		var member = message.guild.members.get(mutee[k].id);
+		var user = bot.users.get(mutee[k].id);
+		var guild = message.guild;
+		var channels = message.guild.channels.array();
+		for (var i = 0; i < channels.length; i++) {
+			if (channels[i].type == 'text')
+				channels[i].overwritePermissions(member, {
+					SEND_MESSAGES: null
+				})
+		};
+		var unmute = new Discord.RichEmbed();
+		unmute.setTitle('User was unmuted.')
+			.addField('User', user)
+			.addField('Unmuted by', message.author)
+			.setFooter("UNMUTED")
+			.setTimestamp()
+			.setColor([Math.floor(Math.random() * 256), 255, Math.floor(Math.random() * 256)])
+		message.channel.send({
+			embed: unmute
+		})
+	};
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['unshush'],
-    botPerms: ['MANAGE_ROLES'],
-    memberPerms: ['MANAGE_ROLES']
+	enabled: true,
+	guildOnly: false,
+	aliases: ['unshush'],
+	botPerms: ['MANAGE_ROLES'],
+	memberPerms: ['MANAGE_ROLES']
 };
 
 exports.help = {
-    name: 'unmute',
-    description: 'Unmutes a member that has been muted',
-    usage: 'unmute @<member>'
+	name: 'unmute',
+	description: 'Unmutes a member that has been muted',
+	usage: 'unmute @<member>'
 };
