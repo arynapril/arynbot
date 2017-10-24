006d70f7e98409e368439995f1b33af5b0c39cad
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:39:45 -0400

    Update pickupline.js

--- commands/pickupline.js
+++ commands/pickupline.js
@@ -1,20 +1,20 @@
 exports.run = (bot, message, args, level) => {
-const MarkovChain = require('markovchain');
-const fs = require('fs');
-const pickuplines = new MarkovChain(fs.readFileSync('./pickuplines.txt', 'utf8'));
-message.channel.send(`${pickuplines.start("Hey").end("Zach").process()}`);
+	const MarkovChain = require('markovchain');
+	const fs = require('fs');
+	const pickuplines = new MarkovChain(fs.readFileSync('./pickuplines.txt', 'utf8'));
+	message.channel.send(`${pickuplines.start("Hey").end("Zach").process()}`);
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    botPerms: [],
-    memberPerms: []
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	botPerms: [],
+	memberPerms: []
 };
 
 exports.help = {
-    name: 'pickupline',
-    description: 'Generates a random, nonsensical pickup line.',
-    usage: 'pickupline <@person being flirted with [optional]>'
+	name: 'pickupline',
+	description: 'Generates a random, nonsensical pickup line.',
+	usage: 'pickupline <@person being flirted with [optional]>'
 };
