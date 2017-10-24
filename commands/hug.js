1c1b00ba9c33fd58e7d48a50dbd5348e5fdd08ec
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:30:48 -0400

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,22 +1,22 @@
 exports.run = (bot, message, args, level) => {
-    if (!message.mentions.users.array()[0]) {
-        message.channel.send(`*hugs* ***${message.author.username}***`)
-    } else {
-        let huggee = message.mentions.users.array()[0];
-        message.channel.send(`*hugs* ***${huggee.username}***`);
-    };
+	if (!message.mentions.users.array()[0]) {
+		message.channel.send(`*hugs* ***${message.author.username}***`)
+	} else {
+		let huggee = message.mentions.users.array()[0];
+		message.channel.send(`*hugs* ***${huggee.username}***`);
+	};
 };
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['hugs', 'hugz'],
-    botPerms: [],
-    memberPerms: []
+	enabled: true,
+	guildOnly: false,
+	aliases: ['hugs', 'hugz'],
+	botPerms: [],
+	memberPerms: []
 };
 
 exports.help = {
-    name: 'hug',
-    description: 'Everyone needs a hug sometimes.',
-    usage: 'hug <@person needing a hug [optional]>'
+	name: 'hug',
+	description: 'Everyone needs a hug sometimes.',
+	usage: 'hug <@person needing a hug [optional]>'
 };
