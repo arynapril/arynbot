ad88e33061877309aab1dd323ffa8beed27d5425
Michael Cao <mike.x.cao@gmail.com>
2017-10-24 11:45:34 -0400

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,9 +1,9 @@
 module.exports = (bot, oldMember, newMember) => {
-    let role = newMember.guild.roles.find("name", 'voice');
-    if (!role) return;
-    if (newMember.voiceChannel) {
-        newMember.addRole(role);
-    } else {
-        newMember.removeRole(role);
-    };
+	let role = newMember.guild.roles.find("name", 'voice');
+	if (!role) return;
+	if (newMember.voiceChannel) {
+		newMember.addRole(role);
+	} else {
+		newMember.removeRole(role);
+	};
 };
