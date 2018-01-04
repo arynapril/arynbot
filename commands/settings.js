187bcf28ba144a6f8313980559491e9de802f78f
Aryn Thomas <arynapril@gmail.com>
2018-01-04 00:39:18 -0500

    im a dumbasssss

--- commands/settings.js
+++ commands/settings.js
@@ -21,7 +21,8 @@ exports.run = async (bot, message, args, level) => {
 			message.channel.send(`The current **${args[0]}** setting is **${setting}**`)
 		} else {
 			if (!message.member.hasPermission('MANAGE_GUILD')) return message.reply("you do not have permission to manage this server's setings!");
-			if (args[1] == 'welcomeMessage') {
+			if (args[2] && args[0] !== 'welcomeMessage') return message.channel.send('Please enter only one value!')
+			if (args[0] == 'welcomeMessage') {
 				welcomeMessage = args.join(' ');
 			}
 			if (booleanArray.indexOf(args[0]) != -1 && args[1] !== '1' && args[1] !== '0') return message.channel.send(`The ${args[0]} value must be be either a 0 or a 1! Please try again!`);
