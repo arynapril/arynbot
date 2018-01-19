ff6caf6d2d855f3f5b662cccb97a5ee77ac85e4c
Aryn Thomas <arynapril@gmail.com>
2018-01-18 21:35:02 -0500

    well lets try this

--- commands/exec.js
+++ commands/exec.js
@@ -1,24 +1,23 @@
 exports.run = (bot, message, args, level) => {
     var Discord = require('discord.js');
+    const child_process = require('child_process');
     if (message.author.id !== bot.config.owner) return message.channel.send("You do not have permission to run this command!")
     command = args.join(' ')
     var embed = new Discord.RichEmbed()
         .setFooter(`${message.author.username}`, `${message.author.avatarURL}`)
         .setTimestamp()
         .addField('Command', "```sh\n" + command + "```")
-        require("child_process").exec(command, (err, stdout, stderr) => {
-            if (err) {
-                embed.setColor([255, Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
-                    .setTitle("Error")
-                    .addField('Error', "```sh\n" + stderr + "```")
-                    .addField('Result', "```sh\n" + stdout + "```");
-            } else {
-                embed.setColor([Math.floor(Math.random() * 256), 255, Math.floor(Math.random() * 256)])
-                    .setTitle("Success")
-                    .addField('Result', "```sh\n" + stdout + "```");
-            }
-                message.channel.send({ embed: embed })
-        })
+    const { exec } = require('child_process');
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            embed.setColor([255, Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
+            .addField('Error:', error)
+            .addField(' - ', stderr)
+        } else {
+            embed.setColor([Math.floor(Math.random() * 256), 255, Math.floor(Math.random() * 256)])
+            .addField('Output: ', stdout)
+        }
+    });
 };
 
 exports.conf = {
