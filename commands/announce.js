1b60d38b2a0c57a335ff823148519c33af4341b5
Aryn Thomas <arynapril@gmail.com>
2018-07-21 01:00:58 -0400

    this bitch pushing untested code YEET

---
+++ commands/announce.js
@@ -0,0 +1,40 @@
+exports.run = (bot, message, args, level) => {
+    parts = args.join(' ').split('|');
+    if (message.guild.channels.get(parts[0])){
+        chan = message.guild.channels.get(parts[0]);
+        parts = parts.splice(1); 
+    } else {
+        chan = message.channel
+    };
+    role = message.guild.roles.find('name', parts[0]);
+    if (!role) {
+        message.channel.send("That role was not found! Sorry!")
+    }
+    parts = parts.splice(1);
+    announcement = parts[0];
+    if (role.comparePositionTo(message.member.highestRole) < 0){
+        bool = role.mentionable;
+        role.setMentionable(true, "Announce Command");
+    } else {
+        message.channel.send("That role is above yours, so you cant use this command to ping it!")
+    } 
+    message.delete();
+    chan.send(`From ${message.author}:\n
+               <@&${role.id}>\n
+               ${announcement}`);
+    role.setMentionable(bool, "Announce Command");
+};
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: [],
+	botPerms: ["MANAGE_MESSAGES"],
+	memberPerms: ["MANAGE_ROLES"]
+};
+
+exports.help = {
+	name: 'announce',
+	description: 'make an announcement in a channel',
+	usage: 'announce <optional channel id> | <role name to ping> | announcement'
+};
