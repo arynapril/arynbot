7945f66cf4c531baa30d39b02e226652dd33ad86
Aryn Thomas <arynapril@gmail.com>
2018-07-21 12:56:18 -0400

    adds a check for if its higher than the bots highest role

--- commands/announce.js
+++ commands/announce.js
@@ -12,11 +12,11 @@ exports.run = (bot, message, args, level) => {
     }
     parts = parts.splice(1);
     announcement = parts[0];
-    if (role.comparePositionTo(message.member.highestRole) < 0){
+    if (role.comparePositionTo(message.member.highestRole) < 0 || role.comparePositionTo(message.guild.me.highestRole) < 0){
         bool = role.mentionable;
         role.setMentionable(true, "Announce Command");
     } else {
-        return message.channel.send("That role is above yours, so you cant use this command to ping it!")
+        return message.channel.send("That role is above your and/or my highest role, so you cant use this command to ping it!")
     } 
     message.delete();
     chan.send(`From ${message.author}:\n<@&${role.id}>\n${announcement}`);
