16342e8ad9b421d95d257d284dec1237664e70bf
Aryn Thomas <arynapril@gmail.com>
2018-08-06 00:26:47 -0400

    finally fixed this

--- commands/serverlist.js
+++ commands/serverlist.js
@@ -2,7 +2,7 @@ exports.run = (bot, message, args, level) => {
     var str = ""; 
     var guilds = bot.guilds.array(); 
     for (var i = 0; i < guilds.length; i++) { 
-        str += `${i} - ${guilds[i].name.replace('@', '@ ')}\n`; 
+        str += `${i} - ${guilds[i].name.replace(/@/g, '@ ')}\n`; 
     } 
     message.channel.send(str);
 };
