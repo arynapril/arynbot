41867e6cc1b9065e6426307854460d97141acd29
Aryn Thomas <arynapril@gmail.com>
2018-09-28 12:42:07 -0400

    fixed hug undefined error

--- commands/hug.js
+++ commands/hug.js
@@ -1,6 +1,6 @@
 exports.run = (bot, message, args, level) => {
 	if (!message.mentions.users.array()[0]) {
-		message.channel.send(`*hugs* ***${message.username}***`)
+		message.channel.send(`*hugs* ***${message.author.username}***`)
 	} else {
 		let huggee = message.mentions.users.array()[0];
 		message.channel.send(`*hugs* ***${huggee.username}***`);
