d53f6058e54ebb88682f4d31ecb02d2ad50607ae
Aryn Thomas <arynapril@gmail.com>
2019-05-15 10:26:00 -0400

    yam mega dumb

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,7 +2,7 @@ module.exports = async (bot, member) => {
 	bot.log("log", `${member.guild.name} lost a member - ${member.user.username} (${member.id})`, 'MEMBR');
 	enabled = await bot.getSetting('leaveMessagesEnabled', member.guild);
 	if (enabled == '0') return;
-	setting = await bot.getSetting('leaveeMessagesChannel', member.guild);
+	setting = await bot.getSetting('leaveMessagesChannel', member.guild);
 	welcome = member.guild.channels.find('name', setting);
 	if (!welcome) return;
 	autodelete = await bot.getSetting('leaveMessagesAutoDelete', member.guild);
