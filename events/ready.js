3bc1350057782071c482d8be04a73f719450d7c1
Aryn Thomas <arynapril@gmail.com>
2019-05-19 11:05:04 -0400

    presence fix ideally

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ module.exports = async bot => {
 	await wait(1000);
 	bot.syncServers();
 	bot.log("log", `Ready to love ${bot.users.size} users in ${bot.guilds.size} servers!`, "READY");
-	bot.setPresence({ game: { name: `in ${bot.guilds.size} servers! ❤️` }, status: 'online' })
+	bot.user.setPresence({ game: { name: `in ${bot.guilds.size} servers! ❤️` }, status: 'online' })
 }
