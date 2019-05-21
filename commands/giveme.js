f838287d04d18da553c938a46ba1e2455f8e95fe
Aryn Thomas <arynapril@gmail.com>
2019-05-20 21:11:30 -0400

    option to delete givemes

--- commands/giveme.js
+++ commands/giveme.js
@@ -70,7 +70,22 @@ exports.run = async (bot, message, args, level) => {
         if (removed > 0) removeEmbed.addField(`Removed ${removed} roles!`, removedNames);
         if (didntHave >0) removeEmbed.addField(`You didn't have ${didntHave} roles!`, didntHaveNames);
         if (couldnt > 0) removeEmbed.addField(`Couldn't remove ${couldnt} roles!`, 'The roles requested either don\'t exist or aren\'t part of the roles able to be removed with the bot. To show a list of the roles able to be removed, run \`!giveme list\`');
-        return message.channel.send({embed: removeEmbed});
+        var removeSent = await message.channel.send({embed: removeEmbed});
+        removeSent.react('❌')
+        var collector = removeSent.createReactionCollector(
+            (reaction, user) => reaction.emoji.name == '❌' && user.id == message.author.id,
+            { time: 30000 }
+        );
+        collector.on('collect', m => {
+            message.delete();
+            removeSent.delete();
+            collector.stop();
+        });
+        collector.on('end', collected => {
+            if (collected.size == 0) {
+                removeSent.clearReactions();
+            }
+        });    
     } else {
         addRoles = args.join(" ").split(', ');
         roles = list.split('|');
@@ -111,7 +126,22 @@ exports.run = async (bot, message, args, level) => {
         if (added > 0) addEmbed.addField(`Added ${added} roles!`, addedNames);
         if (alreadyHad >0) addEmbed.addField(`You already had ${alreadyHad} roles!`, alreadyHadNames);
         if (couldnt > 0) addEmbed.addField(`Couldn't add ${couldnt} roles!`, 'The roles requested either don\'t exist, aren\'t part of the roles able to be added with the bot, or I don\'t have adequate perms. To show a list of the roles able to be added, run \`!giveme list\`');
-        return message.channel.send({embed: addEmbed});
+        var sent = await message.channel.send({embed: addEmbed});
+        sent.react('❌')
+        var collector = sent.createReactionCollector(
+            (reaction, user) => reaction.emoji.name == '❌' && user.id == message.author.id,
+            { time: 30000 }
+        );
+        collector.on('collect', m => {
+            message.delete();
+            sent.delete();
+            collector.stop();
+        });
+        collector.on('end', collected => {
+            if (collected.size == 0) {
+                sent.clearReactions();
+            }
+        });
     }
 };
 
